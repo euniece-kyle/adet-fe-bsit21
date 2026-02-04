46b9319c7083effc088c5862a468fdd3fe19981c
euniece-kyle <ekpuerto89843@liceo.edu.ph>
2026-02-04 23:51:43 +0800

    posts.component.ts

---
+++ src/posts/posts.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  standalone: true,
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {}
